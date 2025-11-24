291bbc6766d9b4498b0539ac2d8d146cc83f8cde
rbucher04 <rbucher04@gmail.com>
2025-11-24 04:29:30 -0600

    Change contact API endpoint to production URL
    Updated the API endpoint for adding contacts to use the new backend URL.

--- src/AddContact.jsx
+++ src/AddContact.jsx
@@ -13,7 +13,7 @@ export default function AddContact() {
       return;
     }
     try {
-      const res = await fetch("http://localhost:8081/contacts", {
+      const res = await fetch("https://cs3870-backend-i.onrender.com/contacts", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
@@ -85,3 +85,4 @@ export default function AddContact() {
     </div>
   );
 }
+
